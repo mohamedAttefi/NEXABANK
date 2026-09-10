c948b1d54d570394279e0264be50c52e3dd118a4
mohamedAttefi <attefimohamed0@gmail.com>
2026-09-10 14:10:48 +0100

    create exeption to handle solde exeptions

---
+++ src/com/nexabank/exeption/SoldeInsuffisantException.java
@@ -0,0 +1,7 @@
+package com.nexabank.exeption;
+
+public class SoldeInsuffisantException extends Exception {
+    public SoldeInsuffisantException(String message) {
+        super(message);
+    }
+}

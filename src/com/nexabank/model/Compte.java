eea2fab1b9dd57484f294f86a3fdf25856aad33b
mohamedAttefi <attefimohamed0@gmail.com>
2026-09-10 14:06:10 +0100

    add compte class

---
+++ src/com/nexabank/model/Compte.java
@@ -0,0 +1,41 @@
+package com.nexabank.model;
+
+import java.util.HashSet;
+
+public class Compte {
+    public   static int cpt = 1000;
+    private String numeroCompte;
+    private double solde;
+    private TypeCompte typeCompte;
+
+    private HashSet<Transaction> historiqueTransactions;
+
+    public Compte(String numeroCompte, double solde, TypeCompte typeCompte) {
+
+        this.numeroCompte = numeroCompte;
+        this.solde = solde;
+        this.typeCompte = typeCompte;
+
+        this.historiqueTransactions = new HashSet<>();
+    }
+
+    public String getNumeroCompte() {
+        return numeroCompte;
+    }
+
+    public double getSolde() {
+        return solde;
+    }
+
+    public TypeCompte getTypeCompte() {
+        return typeCompte;
+    }
+
+    public HashSet<Transaction> getHistoriqueTransactions() {
+        return historiqueTransactions;
+    }
+
+    public void setSolde(double solde) {
+        this.solde = solde;
+    }
+}

68973194a511d7629f6ba566230424d2999aecf3
mohamedAttefi <attefimohamed0@gmail.com>
2026-09-10 14:05:59 +0100

    add client service to manage all client dependent functionalities class

---
+++ src/com/nexabank/service/ClientService.java
@@ -0,0 +1,39 @@
+// File: src/com/nexabank/service/ClientService.java
+package com.nexabank.service;
+
+import com.nexabank.model.Client;
+import com.nexabank.model.Personne;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class ClientService {
+    private ArrayList<Personne> clientsDb = LoginService.users;
+
+    public void afficherClients() {
+        for (int i = 0; i < clientsDb.size(); i++) {
+            if (clientsDb.get(i) instanceof Client) {
+                Client client = (Client) clientsDb.get(i);
+                System.out.println(
+                        "ID: " + client.getIdClient()
+                                + " | Nom: " + client.getNom()
+                                + " " + client.getPrenom()
+                                + " | Email: " + client.getEmail()
+                );
+            }
+        }
+    }
+
+    public Client trouverParId(int idClient) {
+        return (Client) clientsDb.get(idClient);
+    }
+
+    public void mettreAJourInfos(int idClient, String nouveauNom, String nouveauPrenom, String nouveauEmail) {
+        Client client = (Client) clientsDb.get(idClient);
+        if (client != null) {
+            client.setNom(nouveauNom);
+            client.setPrenom(nouveauPrenom);
+            client.setEmail(nouveauEmail);
+        }
+    }
+}
